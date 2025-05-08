b41bdfd23a1273308c41d8fbdc64bde57ff22a64
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-05-08 19:18:23 +0200

    creato AuthController e implementato metodo per registrare un nuovo utente

---
+++ src/main/java/luca/engineer/controllers/AuthController.java
@@ -0,0 +1,28 @@
+package luca.engineer.controllers;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import luca.engineer.dto.ParamRegisterUser;
+import luca.engineer.services.UserService;
+
+@RestController
+@CrossOrigin(origins="*")
+public class AuthController {
+
+	@Autowired
+	UserService userService;
+	
+	@PostMapping("/api/user/register")
+	public HashMap<String, Boolean> registerUser(@RequestBody ParamRegisterUser json) throws Exception {
+		HashMap<String, Boolean> response = new HashMap<>();
+		response.put("response",userService.registerUser(json));
+		return response;
+	}
+	
+}
--- src/main/java/luca/engineer/security/SecurityConfig.java
+++ src/main/java/luca/engineer/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package luca.engineer.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,12 +16,13 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
 		http
-			.cors(Customizer.withDefaults())
-			.csrf(Customizer.withDefaults())
+			.cors(cors -> cors.disable())
+			.csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests(req -> {
 				req.requestMatchers("/").permitAll();
 				req.requestMatchers("/login").permitAll();
 				req.requestMatchers("/register").permitAll();
+				req.requestMatchers(HttpMethod.POST,"/api/user/register").permitAll();
 				req.requestMatchers("/app/**").authenticated();
 				req.requestMatchers("/css/**", "/js/**", "/img/**", "/webjars/**").permitAll();
 			});
