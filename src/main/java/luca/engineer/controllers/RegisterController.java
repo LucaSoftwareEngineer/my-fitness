c15a9529487879b632a85e0c372bc1fa83009ad6
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-05-08 20:43:07 +0200

    rinominato AuthContreller in RegisterController, poichè l'autenticazione la faccio gestire a spring security

--- src/main/java/luca/engineer/controllers/RegisterController.java
+++ src/main/java/luca/engineer/controllers/RegisterController.java
@@ -3,7 +3,6 @@ package luca.engineer.controllers;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +11,7 @@ import luca.engineer.dto.ParamRegisterUser;
 import luca.engineer.services.UserService;
 
 @RestController
-@CrossOrigin(origins="*")
-public class AuthController {
+public class RegisterController {
 	
 	@Autowired
 	UserService userService;
